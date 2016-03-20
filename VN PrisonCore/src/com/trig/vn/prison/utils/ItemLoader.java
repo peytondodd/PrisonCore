c88672d213333e6f9a16bbc1373e02c7ea5d186b
Steven <steven.simko1000@gmail.com>
2016-03-20 14:53:15 -0500

    Added debug message

--- VN PrisonCore/src/com/trig/vn/prison/utils/ItemLoader.java
+++ VN PrisonCore/src/com/trig/vn/prison/utils/ItemLoader.java
@@ -6,14 +6,15 @@ import org.bukkit.material.MaterialData;
 public class ItemLoader {
 
 	public static ItemStack loadItem(String item) {
+		System.out.println("Loading item: " + item);
 		ItemStack itemstack = null;
 		if(item.contains("\\:")) {
 			String[] parts = item.split("\\:");
 			int itemId = Integer.parseInt(parts[0]);
 			int magic = Integer.parseInt(parts[1]);
-//			System.out.println("Parsed item: ");
-//			System.out.println("Item ID: " + itemId);
-//			System.out.println("Data Value: " + magic);
+			System.out.println("Parsed item: ");
+			System.out.println("Item ID: " + itemId);
+			System.out.println("Data Value: " + magic);
 			itemstack = new ItemStack(itemId);
 			MaterialData data = itemstack.getData();
 			data.setData((byte) magic);
