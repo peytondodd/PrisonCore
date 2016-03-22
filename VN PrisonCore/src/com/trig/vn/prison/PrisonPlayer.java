9d0854ebaf371216a622576a16cb73f4722bfba9
Steven <steven.simko1000@gmail.com>
2016-03-22 17:44:50 -0500

    Moved PrisonPlayer class Added player achievements

---
+++ VN PrisonCore/src/com/trig/vn/prison/PrisonPlayer.java
@@ -0,0 +1,39 @@
+package com.trig.vn.prison;
+
+import net.minecraft.server.v1_9_R1.EntityPlayer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_9_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.inventory.Inventory;
+
+import com.trig.vn.prison.ranks.PrisonRank;
+
+public class PrisonPlayer extends CraftPlayer {
+
+	private PrisonRank rank;
+	private Inventory altInv;
+	private PrisonAchievements achievements = new PrisonAchievements();
+	
+	public PrisonPlayer(CraftServer server, EntityPlayer entity) {
+		super(server, entity);
+		altInv = Bukkit.getServer().createInventory(null, 54, "" + getName());
+	}
+
+	public PrisonRank getRank() {
+		return rank;
+	}
+
+	public void setRank(PrisonRank rank) {
+		this.rank = rank;
+	}
+	
+	public Inventory getAlternativeInventory() {
+		return altInv;
+	}
+	
+	public PrisonAchievements getAchievements() {
+		return achievements;
+	}
+	
+}
