e1c3b94d88a0baa6edd618acbd0384f03a4312ad
Steven <steven.simko1000@gmail.com>
2016-04-24 16:24:58 -0500

    Made lightning faster

--- VN PrisonCore/src/com/trig/vn/prison/utils/SpawnTools.java
+++ VN PrisonCore/src/com/trig/vn/prison/utils/SpawnTools.java
@@ -22,7 +22,7 @@ public class SpawnTools {
 					center.getWorld().strikeLightningEffect(new Location(center.getWorld(), center.getX() + temp, center.getY(), center.getZ()));
 					temp -= 2;
 				}
-			}, 10 * iterations);
+			}, 4 * iterations);
 		}
 		
 	}
