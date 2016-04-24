656c5d8dc29d9ebd0f6602f0573fb280cb470435
Steven <steven.simko1000@gmail.com>
2016-04-24 16:24:10 -0500

    Tests

--- VN PrisonCore/src/com/trig/vn/prison/utils/SpawnTools.java
+++ VN PrisonCore/src/com/trig/vn/prison/utils/SpawnTools.java
@@ -8,21 +8,21 @@ import com.trig.vn.prison.Prison;
 public class SpawnTools {
 
 	private static int temp = 100;
+	private static int iterations = 0;
 	
 	public static void lightningCircle() {
 		temp = 100;
+		iterations = 0;
 		final Location center = new Location(Bukkit.getServer().getWorld("prison"), -60, 200, -60);
-		int iterations = 0;
 		for(int i = 0; i < 50; i++) {
 			iterations++;
 			Bukkit.getServer().getScheduler().runTaskLater(Prison.instance(), new Runnable() {
 				public void run() {
 					center.getWorld().strikeLightningEffect(new Location(center.getWorld(), center.getX() - temp, center.getY(), center.getZ()));
 					center.getWorld().strikeLightningEffect(new Location(center.getWorld(), center.getX() + temp, center.getY(), center.getZ()));
-					
+					temp -= 2;
 				}
 			}, 10 * iterations);
-			temp -= 2;
 		}
 		
 	}
