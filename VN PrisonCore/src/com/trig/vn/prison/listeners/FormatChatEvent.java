21239bc14a9690c5976432543335917f769182c3
Steven <steven.simko1000@gmail.com>
2016-06-11 13:57:34 -0500

    Fixed chat format

--- VN PrisonCore/src/com/trig/vn/prison/listeners/FormatChatEvent.java
+++ VN PrisonCore/src/com/trig/vn/prison/listeners/FormatChatEvent.java
@@ -4,8 +4,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import ru.tehkode.permissions.bukkit.PermissionsEx;
-
 import com.trig.vn.prison.Prison;
 import com.trig.vn.prison.PrisonPlayer;
 
@@ -14,6 +12,6 @@ public class FormatChatEvent implements Listener {
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent e) {
 		PrisonPlayer p = Prison.getPrisonManager().getPrisonPlayer(e.getPlayer());
-		e.setFormat("§8[§6" + p.getRank().toString() + "§8] " + PermissionsEx.getUser(p).getPrefix() + " " + p.getDisplayName() + "§7:");
+		e.setFormat("§8[§6" + p.getRank().getName() + "§8] " + p.getDisplayName() + "§7: ");
 	}
 }
