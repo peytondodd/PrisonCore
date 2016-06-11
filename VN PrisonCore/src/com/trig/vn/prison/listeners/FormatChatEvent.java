c05846493cddb33862ace2f0f4db0475ba08d881
Steven <steven.simko1000@gmail.com>
2016-06-11 14:06:57 -0500

    Forgot to add message

--- VN PrisonCore/src/com/trig/vn/prison/listeners/FormatChatEvent.java
+++ VN PrisonCore/src/com/trig/vn/prison/listeners/FormatChatEvent.java
@@ -6,12 +6,42 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import com.trig.vn.prison.Prison;
 import com.trig.vn.prison.PrisonPlayer;
+import com.trig.vn.prison.ranks.PrisonRank;
 
 public class FormatChatEvent implements Listener {
 
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent e) {
 		PrisonPlayer p = Prison.getPrisonManager().getPrisonPlayer(e.getPlayer());
-		e.setFormat("§6" + p.getRank().getName() + " " + p.getDisplayName() + "§7: ");
+		e.setFormat(getColourForRank(p.getRank()) + p.getRank().getName() + " " + p.getDisplayName() + "§7: " + e.getMessage());
+	}
+	
+	private String getColourForRank(PrisonRank rank) {
+		int r = Integer.parseInt(rank.getName());
+		switch(r) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+			return "§7";
+		default:
+			return "§7";
+		}
 	}
 }
